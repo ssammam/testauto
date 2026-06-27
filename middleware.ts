3e988460345270c05f0a799bca82a8d20a8864fe
NONAME-OP <saikiran2425k@gmail.com>
2026-06-27 10:48:25 +0530

    chore: configure custom sign-in page and secret in auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,11 @@
 import { withAuth } from "next-auth/middleware";
 
-export default withAuth;
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
 
 export const config = {
   matcher: [
