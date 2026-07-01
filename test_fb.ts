ee05daf8baded0f68d4fceb4f4fc1d8ef2c3a708
NONAME-OP <saikiran2425k@gmail.com>
2026-07-01 13:13:05 +0530

    chore: remove unused Sanity and Facebook API test scripts

--- test_fb.ts
+++
@@ -1,8 +0,0 @@
-const fbToken = process.env.FACEBOOK_PAGE_ACCESS_TOKEN;
-const fbPageId = process.env.FACEBOOK_PAGE_ID;
-async function test() {
-  const fbUrl = `https://graph.facebook.com/v20.0/${fbPageId}/posts?fields=id,message,created_time,full_picture&access_token=${fbToken}&limit=5`;
-  const res = await fetch(fbUrl);
-  console.log(await res.json());
-}
-test();
